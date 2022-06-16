985840b4aea3d3828bbf4bd51ec241a4890d30b3
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-16 10:12:24 -0700

    Implement GoblinArmy component
    The handler is stubbed out for the time being.

--- app.js
+++ app.js
@@ -2,16 +2,26 @@
 
 // import component creators
 import createCharacter from './components/Character.js';
+import createGoblinArmy from './components/GoblinArmy.js';
 
 // import state and dispatch functions
-import state from './state.js';
+import state, { updateGoblin } from './state.js';
 
 // Create each component:
 const Character = createCharacter(document.querySelector('.character'));
+const GoblinArmy = createGoblinArmy(document.querySelector('.goblin-army'), {
+    handleCombatRound(goblin) {
+        // TODO: Implement actual game logic
+        goblin.hp--;
+        updateGoblin(goblin);
+        display();
+    },
+});
 
 // Roll-up display function that renders (calls with state) each component
 function display() {
     Character({ character: state.character });
+    GoblinArmy({ goblins: state.goblins });
 }
 
 // Call display on page load
