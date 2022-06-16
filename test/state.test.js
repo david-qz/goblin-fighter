df9fee4da15e0c6fb314933da81bfd6ec2f647aa
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-16 09:53:00 -0700

    Test and implement state actions

--- test/state.test.js
+++ test/state.test.js
@@ -1,6 +1,9 @@
 import state, {
     initialize,
-    // import dispatch functions
+    addGoblin,
+    updateGoblin,
+    updatePlayer,
+    addMessage,
 } from '../state.js';
 
 // make sure state is at known starting point
@@ -8,14 +11,58 @@ QUnit.module('state', { beforeEach: initialize });
 
 const test = QUnit.test;
 
-test('the first state test...', (expect) => {
-    // what is the initial expected state?
+test('Initial state', (expect) => {
+    expect.deepEqual(state.character, {
+        hp: 10,
+        goblinsDefeated: 0,
+    });
 
-    // use the action
+    expect.equal(state.goblins.length, 2);
+    for (const goblin of state.goblins) {
+        expect.equal(typeof goblin.name, 'string');
+        expect.equal(goblin.hp, 10);
+    }
 
-    // what should the state be now?
+    expect.deepEqual(state.log, []);
+});
 
+test('addGoblin()', expect => {
+    addGoblin({
+        hp: 99,
+        name: 'Boss',
+    });
+    const lastGoblin = state.goblins[state.goblins.length - 1];
 
-    // remove this line when starting your test
-    expect.deepEqual(state, {});
+    expect.deepEqual(lastGoblin, {
+        hp: 99,
+        name: 'Boss',
+    });
+});
+
+test('updateGoblin()', expect => {
+    const goblin = state.goblins[0];
+    goblin.hp = 5;
+    updateGoblin(goblin);
+
+    expect.equal(state.goblins[0].hp, 5);
+});
+
+test('updatePlayer()', expect => {
+    const character = state.character;
+    character.hp = 3;
+    updatePlayer(character);
+
+    expect.equal(state.character.hp, 3);
+});
+
+test('addMessage()', expect => {
+    addMessage('You attacked Jerry for 4 points of damage.');
+    addMessage('Jerry attacked you for 10 points of damage.');
+    addMessage('You died.');
+
+    expect.deepEqual(state.log, [
+        'You attacked Jerry for 4 points of damage.',
+        'Jerry attacked you for 10 points of damage.',
+        'You died.',
+    ]);
 });
