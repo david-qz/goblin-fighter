0280afd2f09e819557bd80cffbbb25cc43c8bf1b
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-17 10:08:35 -0700

    Make goblins appear automatically
    - New goblins have a chance of spawing on every attack.
    - A new goblin will always spawn if the battlefield is empty.
    - Goblins can spawn in any of the eight grid locations and will
     stay where they are.
    - The goblins array is now sparse and the code for that is not
     great. I really need a battlefield class that encapsulates all
     the implementation details... and a goblin class... a game class.

--- components/Goblin.js
+++ components/Goblin.js
@@ -1,18 +1,23 @@
 export default function createGoblin(goblin) {
     const div = document.createElement('div');
-    div.classList.add('goblin');
 
-    const nameSpan = document.createElement('span');
-    nameSpan.classList.add('name-tag');
-    nameSpan.textContent = goblin.name;
+    // If no goblin, emit an empty div to take up the grid space
+    if (goblin) {
+        div.classList.add('goblin');
 
-    const img = document.createElement('img');
-    img.src = goblin.defeated ? './assets/goblin-defeated.png' : './assets/goblin.png';
+        const nameSpan = document.createElement('span');
+        nameSpan.classList.add('name-tag');
+        nameSpan.textContent = goblin.name;
 
-    const hpSpan = document.createElement('span');
-    hpSpan.classList.add('hp');
-    hpSpan.textContent = goblin.hp;
+        const img = document.createElement('img');
+        img.src = goblin.defeated ? './assets/goblin-defeated.png' : './assets/goblin.png';
 
-    div.append(nameSpan, img, hpSpan);
+        const hpSpan = document.createElement('span');
+        hpSpan.classList.add('hp');
+        hpSpan.textContent = goblin.hp;
+
+        div.append(nameSpan, img, hpSpan);
+    }
+
     return div;
 }
