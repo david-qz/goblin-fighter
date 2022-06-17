4ca96e32bc013c2bcbe87179ce2cc53f8ea22329
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-17 11:19:49 -0700

    Make defeated goblins partially desaturated

--- components/Goblin.js
+++ components/Goblin.js
@@ -4,6 +4,9 @@ export default function createGoblin(goblin) {
     // If no goblin, emit an empty div to take up the grid space
     if (goblin) {
         div.classList.add('goblin');
+        if (goblin.defeated) {
+            div.classList.add('defeated');
+        }
 
         const nameSpan = document.createElement('span');
         nameSpan.classList.add('name-tag');
