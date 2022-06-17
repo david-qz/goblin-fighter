1c3d2a96a01b42d8f27181c4fc065d8d2bc2654a
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-17 11:46:20 -0700

    Improve game logic structure

--- game.js
+++ game.js
@@ -10,39 +10,43 @@ import {
 export function doCombatRound(character, goblin) {
     if (goblin.defeated || character.defeated) return;
 
+    // Attack the goblin
     const damage = rollDice(3, 4);
     goblin.hp = Math.max(0, goblin.hp - damage);
     updateGoblin(goblin);
     addMessage(`You attacked ${goblin.name} for ${damage} points of damage.`);
 
     if (goblin.defeated) {
+        // The player gets a heal when defeating a goblin
         character.goblinsDefeated++;
         const heal = rollDice(2, 2);
         character.hp += heal;
         addMessage(`You defeated ${goblin.name} and healed for ${heal} hp.`);
-        if (!anyoneLeftStanding()) {
-            const goblin = addGoblin();
-            if (goblin) {
-                addMessage(`${goblin.name} arrives on the battlefield.`);
-            }
-        }
-        return;
-    }
 
-    const damageTaken = rollDice(2, 3);
-    character.hp = Math.max(0, character.hp - damageTaken);
-    updateCharacter(character);
-    addMessage(`${goblin.name} attacked you for ${damageTaken} points of damage.`);
+    } else {
+        // If the goblin survived the player's attack, they get to strike back
+        const damageTaken = rollDice(2, 3);
+        character.hp = Math.max(0, character.hp - damageTaken);
+        updateCharacter(character);
+        addMessage(`${goblin.name} attacked you for ${damageTaken} points of damage.`);
+    }
 
     if (character.defeated) {
         addMessage('You been defeated. GAME OVER!');
-        return;
-    }
-
-    if (rollDice(1, 4) === 1) {
-        const goblin = addGoblin();
-        if (goblin) {
-            addMessage(`${goblin.name} arrives on the battlefield.`);
+    } else {
+        // Spawn some goblins if the battlefield is empty or a dice roll succeeds
+        if (!anyoneLeftStanding() || rollDice(1, 5) === 1) {
+            const numToSpawn = rollDice(1, 2);
+            for (let i = 0; i < numToSpawn; i++) {
+                addAndAnnounceNewGoblin();
+            }
         }
+    }
+}
+
+function addAndAnnounceNewGoblin() {
+    const goblin = addGoblin();
+    if (goblin) {
+        addMessage(`${goblin.name} arrives on the battlefield.`);
     }
 }
