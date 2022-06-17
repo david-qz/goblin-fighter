7ab2bc47400d302b471a7f8eb9372ab29e389efc
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-16 17:09:25 -0700

    Attach GoblinNamer to name queue

--- components/GoblinNamer.js
+++ components/GoblinNamer.js
@@ -1,11 +1,11 @@
-export default function createGoblinNamer(root, { handleAddGoblin }) {
+export default function createGoblinNamer(root, { handleEnqueueName }) {
     const form = root.querySelector('form');
 
     form.addEventListener('submit', e => {
         e.preventDefault();
 
         const formData = new FormData(form);
-        handleAddGoblin(formData.get('name'));
+        handleEnqueueName(formData.get('name'));
 
         form.reset();
     });
